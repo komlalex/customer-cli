b2752f1265c5cc15bcb15aa18a91d68992d93c60
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-05-19 04:30:57 +0000

    Database connection set up

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const app = express();
+const port = process.env.PORT || 1998
+//Connect DB
+connectDB();
+  
+
+
+app.listen(port, () => console.log(`Server started on port: ${port}`))
