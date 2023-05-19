7e1bf012cc120d1eba7bff36562ba0235e2ed0f4
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-05-19 10:11:10 +0000

    Adding and finding customer implementation successfull

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 require("colors");
 const connectDB = require("./config/db");
 
-const app = express();
 const port = process.env.PORT || 1998
 //Connect DB
 connectDB();
+
+// Import model
+const Customer = require("./model/customer");
+
+//Add Customer
+const addCustomer = async (customer) => {
+    try {
+        const newCustomer = new Customer(customer);
+        await newCustomer.save()
+        console.info("New Customer Added");
+        console.info(`Success!!!`.green);
+        process.exit(0);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(0);
+    }
+}
+        
+//Find Customer
+const findCustomer = async (name) => {
+    // Make case insensitive
+    const search = new RegExp(name, "i");
+    try {
+       const customer = await Customer.find({$or: [{firstname: search}, {lastname: search}]});
+        console.info(customer);
+        console.info(`${customer.length} matches`);
+        console.info(`Success!!!`.green);
+        process.exit(0);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(0);
+    }
+}
   
+// Exports All Methods
 
-
-app.listen(port, () => console.log(`Server started on port: ${port}`))
+module.exports = {
+    addCustomer,
+    findCustomer
+}
